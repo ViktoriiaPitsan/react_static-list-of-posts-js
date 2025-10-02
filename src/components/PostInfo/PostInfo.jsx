e918ad5264b99cd7b019069c217ae7e847a722f7
Viktoriia <vikapitsan4@gmail.com>
2025-10-03 00:57:25 +0300

    move styles

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,5 +1,6 @@
-import { UserInfo } from '../UserInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
 import { CommentList } from '../CommentList/CommentList';
+import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => {
   const { title, body, user, comments } = post;
